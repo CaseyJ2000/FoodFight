01229f75d535d5f582a10435308fd700af0e2ea0
Ubuntu <solele1@VM-SE.tfa5o45n2aee1pmna4cmyja3ka.bx.internal.cloudapp.net>
2021-11-13 03:21:52 +0000

    Added search logic without retrieving from form.

--- app.py
+++ app.py
@@ -1,27 +1,38 @@
 """Loads the app"""
 import os
+from flask.templating import render_template
+import requests
 from dotenv import load_dotenv, find_dotenv
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import (login_user, current_user, LoginManager, UserMixin,
-                         login_required)
+from flask_login import (
+    login_user,
+    current_user,
+    LoginManager,
+    UserMixin,
+    login_required,
+)
 import flask
+from requests.api import request
 from werkzeug.security import generate_password_hash, check_password_hash
+
+from yelp import getRestaurant
 
 load_dotenv(find_dotenv())
 
 app = flask.Flask(__name__)
-uri = os.getenv('DATABASE_URL')
+uri = os.getenv("DATABASE_URL")
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = os.getenv('SECRET_KEY')
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = os.getenv("SECRET_KEY")
 
 db = SQLAlchemy(app)
 
 
 class User(UserMixin, db.Model):
     """Creates model for User. Username is the identifier"""
+
     id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String(100))
     username = db.Column(db.String(1000), unique=True)
@@ -47,64 +58,112 @@ def load_user(user_name):
     return User.query.get(user_name)
 
 
-@app.route('/menu')
+@app.route("/menu")
 @login_required
 def menu():
+    # term = "Cheesecake"
+    # location = "NYC"
+    bizname = flask.request.form.get("term")
+    bizlocation = flask.request.form.get("location")
+
+    # search_results = len(term) > 0
+    # if search_results:
+    # restaurantInfo = getRestaurant(search_term, search_location)
+    restaurantInfo = getRestaurant()
+    # (name, image, location) = restaurantInfo()
+    # (song_name, song_artist, song_image_url, preview_url) = get_song_data(artist_id, access_token)
+    name = restaurantInfo[0]  # biz name
+    image = restaurantInfo[1]  # biz image
+    location = restaurantInfo[2]  # biz location
+    # else:
+    #     biz_name = None  # biz name
+    #     biz_image = None  # biz image
+    #     biz_location = None  # biz location
+
     """Loads menu webpage"""
-    return flask.render_template('menu.html')
+    return flask.render_template(
+        "menu.html",
+        image=image,
+        location=location,
+        name=name,
+    )
 
 
-@app.route('/signup', methods=['POST', 'GET'])
+@app.route("/search", methods=["GET", "POST"])
+@login_required
+def search():
+    if flask.request.method == "POST":
+        # store as global variable or pass to method
+        term = flask.request.form.get("term")
+        location = flask.request.form.get("location")
+        try:
+            getRestaurant()
+        except Exception:
+            return flask.render_template("menu.html")
+        return flask.render_template("menu.html", term=term, location=location)
+
+    # getRestaurant()
+    # getRestaurant(term, location)
+    # try:
+    #     getRestaurant(term, location)
+    # except Exception:
+    #     return flask.redirect(flask.url_for("menu"))
+
+    return flask.redirect(flask.url_for("menu"))
+
+
+@app.route("/signup", methods=["POST", "GET"])
 def signup():
     """Endpoint for signup"""
-    if flask.request.method == 'POST':
-        username = flask.request.form.get('username')
-        password = flask.request.form.get('password')
+    if flask.request.method == "POST":
+        username = flask.request.form.get("username")
+        password = flask.request.form.get("password")
 
         user = User.query.filter_by(username=username).first()
         if user:
             flask.flash("Username taken")
-            return flask.redirect(flask.url_for('signup'))
+            return flask.redirect(flask.url_for("signup"))
 
-        new_user = User(username=username,
-                        password=generate_password_hash(password,
-                                                        method='sha256'))
+        new_user = User(
+            username=username,
+            password=generate_password_hash(password, method="sha256"),
+        )
 
         db.session.add(new_user)
         db.session.commit()
 
-        return flask.redirect(flask.url_for('login'))
+        return flask.redirect(flask.url_for("login"))
     else:
-        return flask.render_template('signup.html')
+        return flask.render_template("signup.html")
 
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route("/login", methods=["POST", "GET"])
 def login():
     """Endpoint for login"""
-    if flask.request.method == 'POST':
-        username = flask.request.form.get('username')
-        password = flask.request.form.get('password')
+    if flask.request.method == "POST":
+        username = flask.request.form.get("username")
+        password = flask.request.form.get("password")
 
         user = User.query.filter_by(username=username).first()
 
         if not user or not check_password_hash(user.password, password):
             flask.flash("Username or password bad")
-            return flask.redirect(flask.url_for('login'))
+            return flask.redirect(flask.url_for("login"))
         login_user(user)
-        return flask.redirect(flask.url_for('menu'))
+        return flask.redirect(flask.url_for("menu"))
     else:
-        return flask.render_template('login.html')
+        return flask.render_template("login.html")
 
 
-@app.route('/')
+@app.route("/")
 def main():
     """Origin for user Login"""
     if current_user.is_authenticated:
-        return flask.redirect(flask.url_for('menu'))
-    return flask.redirect(flask.url_for('login'))
+        return flask.redirect(flask.url_for("menu"))
+    return flask.redirect(flask.url_for("login"))
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv("IP", "0.0.0.0"),
-            port=int(os.getenv("PORT", "8081")),
-            debug=True)
+    app.run(
+        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "8081")), debug=True
+    )
--- yelp.py
+++ yelp.py
@@ -1,7 +1,9 @@
 import os
 import requests
 import json
+import flask
 from dotenv import load_dotenv, find_dotenv
+from requests.api import get, request
 
 load_dotenv(find_dotenv())
 
@@ -12,36 +14,118 @@ BASE_URL = "https://api.yelp.com/v3/businesses/search"
 HEADERS = {"Authorization": "Bearer %s" % API_KEY}
 
 # define search parameters (practice with api)
-PARAMETERS = {
-    "term": "",
-    "limit": 10,
-    "offset": 50,
-    "radius": 10000,
-    "location": "Springfield, MA",
-}
+# PARAMETERS = {
+#     "term": "",
+#     "limit": 10,
+#     # "offset": 50,
+#     # "radius": 10000,
+#     "location": "Atlanta",
+# }
 
-response = requests.get(url=BASE_URL, params=PARAMETERS, headers=HEADERS)
+# response = requests.get(url=BASE_URL, params=PARAMETERS, headers=HEADERS)
 
-# Conver the JSON String
-data = response.json()
+# # Convert the JSON String
+# data = response.json()
+# # print(data)
+# for i in range(10):
+#     try:
+#         biz = data["businesses"][i]["name"]
+#         print(biz)
 
-# print the response
+#     except KeyError:
+#         print("Couldn't fetch")
+#         break
+# # print the response
 # print(data)
 # print(data["businesses"])
 
 # for item in data["businesses"]:
-#     print(item["name"])
+#     print(item["name"], item["image_url"], item["location"]["city"])
 
-for i in range(10):
-    try:
-        biz = data["businesses"][i]["name"]
-        print(biz)
-
-    except KeyError:
-        print("Couldn't fetch")
-        break
 
 # create exception for small cities so app doesn't break
 
+
 # def getRestaurant():  # function for getting name of Restuarant
-#     return
+#     PARAMETERS = {
+#         "limit": 10,
+#         "term": "seafood",
+#         "location": "Chicago",
+#     }
+
+#     response = requests.get(url=BASE_URL, params=PARAMETERS, headers=HEADERS)
+
+#     # response = requests.get(
+#     #     f"https://api.yelp.com/v3/businesses/search?term={term}&location={location}",
+#     #     # params={"limit": 10},
+#     #     headers=HEADERS,
+#     # )
+#     # Convert the JSON String
+#     data = response.json()
+#     # print(data)
+#     for i in range(0, 10):
+#         try:
+#             biz = data["businesses"][i]["name"]
+#             print(biz)
+#         except KeyError:
+#             print("Couldn't fetch")
+#             break
+#     # for i in range(0, 20):
+#     biz_name = data["businesses"][i]["name"]
+#     biz_image = data["businesses"][i]["image_url"]
+#     biz_location = data["businesses"][i]["location"]["city"]
+
+#     # print(biz_name, biz_image, biz_location)
+
+#     return (biz_name, biz_image, biz_location)
+
+
+# print(getRestaurant())
+
+# PARAMETERS = {
+#     "limit": 20,
+#     "term": "seafood",
+#     "location": "Chicago",
+# }
+
+
+# def main_request():
+#     response = requests.get(url=BASE_URL, params=PARAMETERS, headers=HEADERS)
+#     return response.json()
+
+
+# def response(r):
+#     return r["businesses"]["name"]
+
+
+def getRestaurant():
+    term = "cheesecake"
+    location = "LA"
+    PARAMETERS = {
+        "limit": 20,
+        "term": term,
+        "location": location,
+    }
+
+    response = requests.get(url=BASE_URL, params=PARAMETERS, headers=HEADERS)
+
+    data = response.json()
+    name = []
+    location = []
+    image = []
+
+    for i in data["businesses"]:
+        name.append(i["name"])
+        location.append(i["location"]["city"])
+        image.append(i["image_url"])
+
+    DATA = {"name": name, "location": location, "image": image}
+    datalist = []
+    datalist.append(DATA)
+    biz_name = name
+    biz_image = image
+    biz_location = location
+    return (biz_name, biz_image, biz_location)
+
+
+# print(getRestaurant())
