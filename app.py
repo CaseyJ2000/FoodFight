6d532031d74c797c373ce53af692ee03a30adac9
Owen Bochner <owenbochner@protonmail.com>
2021-11-14 19:47:56 +0000

    fixed login.html to post to correct location

--- app.py
+++ app.py
@@ -105,6 +105,4 @@ def main():
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv("IP", "0.0.0.0"),
-            port=int(os.getenv("PORT", "8081")),
-            debug=True)
+    app.run(debug=True)
